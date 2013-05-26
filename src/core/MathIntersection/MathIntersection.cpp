f610f78ad30e00fcf1a87639d1ab8fa02aa31362
Agustin Perez Paladini <agudpp@gmail.com>
2013-05-26 02:05:54 -0300

    Main modules changed from directories, and did some other changes, now starting to change the namespaces

---
+++ src/core/MathIntersection/MathIntersection.cpp
@@ -0,0 +1,18 @@
+/*
+ * MathIntersection.cpp
+ *
+ *  Created on: 30/01/2012
+ *      Author: agustin
+ *
+ */
+
+#include "MathIntersection.h"
+
+MathIntersection::MathIntersection() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MathIntersection::~MathIntersection() {
+	// TODO Auto-generated destructor stub
+}
