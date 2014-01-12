a4f1055407a7599b4b5c1c351052647406779d71
hobborg <hobborg@gmail.com>
2014-01-12 17:36:09 -0300

    Creating temp branch to implement MantisBT issue #391

---
+++ src/tools/random_generator/RandomGenerator.cpp
@@ -0,0 +1,23 @@
+/*
+ * RandomGenerator.cpp
+ *
+ *   Created on: Jan 12, 2014
+ *       Author: Carlos E. Budde
+ *      License: GPLv3
+ */
+#include <random_generator/RandomGenerator.h>
+
+namespace tool {
+
+RandomGenerator::RandomGenerator()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+RandomGenerator::~RandomGenerator()
+{
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace tool */
---
+++ src/tools/random_generator/RandomGenerator.h
@@ -0,0 +1,23 @@
+/*
+ * RandomGenerator.h
+ *
+ *   Created on: Jan 12, 2014
+ *       Author: Carlos E. Budde
+ *      License: GPLv3
+ */
+#ifndef RANDOMGENERATOR_H_
+#define RANDOMGENERATOR_H_
+
+namespace tool {
+
+class RandomGenerator
+{
+public:
+    RandomGenerator();
+    virtual
+    ~RandomGenerator();
+};
+
+} /* namespace tool */
+
+#endif /* RANDOMGENERATOR_H_ */
